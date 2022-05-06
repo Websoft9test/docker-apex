98c7d17f63794804ce2cc1460077ae621023bd83
Darren <27513732@qq.com>
2022-05-06 10:34:08 +0800

    Delete cmd.sh

--- cmd.sh
+++
@@ -1,6 +0,0 @@
-# additional settings for startService.sh (container init script)
-
-# Set the default password for ADMIN
-sed -i "s/Welcome_1\" value=\"$APEX_PASSWORD\">/g" /opt/oracle/apex/setapexadmin.sql
-
-echo "CONN_STRING=sys/$DB_ORACLE_PASSWORD@oracledb:$DB_ORACLE_PORT/xepdb1" > /opt/oracle/variables/conn_string.txt
